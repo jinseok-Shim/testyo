c366bdf9b24ff141422a3f1bdafeb9e42dc0f36e
JinseokShim <jinseokshim@shimjinseoks-Mac.local>
2016-06-20 15:57:17 +0900

    add custom slider

--- testTabEffect/testTabEffect/ViewController.m
+++ testTabEffect/testTabEffect/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "TouchView.h"
+#import "CustomSlider.h"
 
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tabRecognizer;
@@ -19,12 +20,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [[self.view viewWithTag:1] addGestureRecognizer:self.tabRecognizer];
+    
+    [[UISlider appearance] setThumbImage:[UIImage imageNamed:@"bar_handle_n.png"] forState:UIControlStateNormal];
+    [[UISlider appearance] setThumbImage:[UIImage imageNamed:@"bar_handle_d.png"] forState:UIControlStateDisabled];
+    [[UISlider appearance] setMinimumTrackImage:[[UIImage imageNamed:@"bar_progress.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0) resizingMode:UIImageResizingModeStretch] forState:UIControlStateNormal];
+    [[UISlider appearance] setMaximumTrackImage:[[UIImage imageNamed:@"bar_bg_n.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0) resizingMode:UIImageResizingModeStretch] forState:UIControlStateNormal];
+    [[UISlider appearance] setMinimumTrackImage:[[UIImage imageNamed:@"bar_bg_d.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0) resizingMode:UIImageResizingModeStretch] forState:UIControlStateDisabled];
+    [[UISlider appearance] setMaximumTrackImage:[[UIImage imageNamed:@"bar_bg_d.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0) resizingMode:UIImageResizingModeStretch] forState:UIControlStateDisabled];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)slider_value_changed:(CustomSlider *)sender {
+    int discreateValue = roundl([sender value]);
+    [sender setValue:(float) discreateValue];
+    NSLog(@"value is %f", (float)discreateValue);
+    [sender setNeedsDisplay];
+}
+
 
 - (IBAction)addTapEvent:(UITapGestureRecognizer *)sender {
     CGPoint point = [sender locationInView:self.view];
